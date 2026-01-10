8071a18e06a0ad3f95c178798ff57583e385c3c7
Md. Anisur Rahman <141425167+SawonAnis@users.noreply.github.com>
2026-01-11 05:59:54 +0600

    data types

---
+++ dart_practice.dart
@@ -0,0 +1,23 @@
+void main(){
+
+  int age=25;
+  var uni= "AIUB";
+  double pi=3.1416;
+
+  String s= "Coding is fun";
+  print("Sawon= "+uni);
+  print("Age= $age");
+  print(s);
+
+  String smiley = '\u263A';
+  String star = '\u2605';
+
+  String musicNote = '\u266B';
+  print("$smiley,$star,$musicNote");
+
+  bool roll;
+  roll=50>20;
+  print(roll);
+
+}
+
