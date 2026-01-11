ccb83f15fe7f1fb3107fbf8ca01b0af558cc80cb
Md. Anisur Rahman <141425167+SawonAnis@users.noreply.github.com>
2026-01-11 08:21:15 +0600

    data types2

---
+++ dart_practice2.dart
@@ -0,0 +1,17 @@
+void main(){
+   var players=["Kaka","Ronaldo","Neymar","Nazario",12,true];
+   print(players[1]);
+   print(players[4]);
+   print(players[5]);
+   print("Players Length : ${players.length}");
+   print("Players: ${players.reversed}");
+   players.addAll(["Messi","D-Maria"]);
+   print(players);
+
+   //Maps
+ var person={"Name":"Sawon","Age":25,"Username":"sawon139"};
+ print(person);
+ person.addAll({"Dept":"CSE"});
+ print(person);
+
+}
